89ec7092b5ed21a49fa66668bc07b6f3eeb7fc67
iwanagahime <eliza.krucon@gmail.com>
2021-03-28 23:11:25 +0100

    add js script file, declare current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 // declaring current time in global memory
-const currentTime = moment().format();
+const currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 
 console.log(currentTime);
