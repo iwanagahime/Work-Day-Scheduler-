6af4304a10669fc5e5adb310c8ad35db41871d25
iwanagahime <eliza.krucon@gmail.com>
2021-03-29 23:34:08 +0100

    get and declare current formated hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
 // declaring current formatted date
 const currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 console.log(currentDay);
-// setting current date to <p>
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
 // A $( document ).ready() block.
 $(document).ready(function () {
+  // setting current date to <p>
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
   console.log("ready!");
 });
+
+const now = new Date(Date.now());
+console.log(now);
+const formatted =
+  now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+console.log(formatted);
+// declaring updateTimeBlocks function
+const updateTimeBlocks = () => {};
