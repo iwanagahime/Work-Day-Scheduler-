147bcef4efa0981551210cd258751d7d57fcc21c
iwanagahime <eliza.krucon@gmail.com>
2021-03-29 23:18:54 +0100

    setting current formated date do p element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-// declaring current time in global memory
-const currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+// declaring current formatted date
+const currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+console.log(currentDay);
+// setting current date to <p>
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
-console.log(currentTime);
+// A $( document ).ready() block.
+$(document).ready(function () {
+  console.log("ready!");
+});
