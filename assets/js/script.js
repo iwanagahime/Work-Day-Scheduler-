714f7ed91df14b056260db901cecbd64ed1f84b6
iwanagahime <eliza.krucon@gmail.com>
2021-04-02 12:45:39 +0100

    declaring and populating timeBlocks array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,42 @@
 // getting current date from moment.js, formatting it and appending to <p> element
 const renderCurrentDate = () => {
   const dateTime = $("#currentDay");
-  displayNow = moment().format("dddd, MMMM Do");
-  console.log(displayNow);
+  const displayNow = moment().format("dddd, MMMM Do");
+
   dateTime.text(displayNow);
 };
 
 // getting data from local storage
 const renderCalendarEvents = () => {
   const calendarEvents = localStorage.getItem("calendarEvents");
-  console.log(calendarEvents);
 
   // if data is present in local storage
   if (calendarEvents !== null) {
-    console.log("TODO- render the data and colors in the table");
+    // TODO render data and colour
     // getting current hour using moment.js
     const currentHour = moment().hour();
-    console.log(currentHour);
+
     // declaring timeBlocksArray
-    const timeBlocks = $("container.row");
+    const timeBlocks = $(".container.row");
     console.log(timeBlocks);
+
+    const callback = function () {
+      const timeBlockTime = $(this).data("time");
+
+      console.log($(this));
+    };
+
+    timeBlocks.each(callback);
     // if there is no data in local storage
   } else {
     localStorage.setItem("calendarEvents", JSON.stringify({}));
   }
 };
+
 // onLoad function
 const onReady = () => {
   renderCurrentDate();
-  console.log("ready!");
+
   renderCalendarEvents();
 };
 $(document).ready(onReady);
